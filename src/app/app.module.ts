739d74c990067b130f9ea8d9440430f6ff8ab857
Alanna <alannapaiva6@gmail.com>
2023-08-12 16:38:40 -0300

    Ajusts of responsivity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { UserDataComponent } from './components/user-data/user-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     SearchBarComponent,
     UserDataComponent
   ],
